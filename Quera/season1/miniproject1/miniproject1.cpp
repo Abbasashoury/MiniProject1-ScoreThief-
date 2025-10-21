4a2cea2a772049abe0a5e392d8813afb03518f9e
Abbas Ashoury <abbasashoury84@gmail.com>
2025-10-21 22:55:45 +0330

    add function1

--- Quera/season1/miniproject1/miniproject1.cpp
+++ Quera/season1/miniproject1/miniproject1.cpp
@@ -12,11 +12,17 @@ struct Student
     Student *next;
 };
 
-void addStudent(Student *&head, const string &name, int studentID, int units, float gpa, const string &securityCode);
-bool deletestudent(Student *&head, int studentID);
-Student *searchStudent(Student *head, int studentID);
+void addStudent(Student *&head);
+bool deletestudent(Student *&head);
+Student *searchStudent(Student *head);
 bool updateStudent(Student *head, int studentID, int newUnits, float newGPA);
 void displayAllRecords(Student *head);
+
+// void addStudent(Student *&head, const string &name, int studentID, int units, float gpa, const string &securityCode);
+// bool deletestudent(Student *&head, int studentID);
+// Student *searchStudent(Student *head, int studentID);
+// bool updateStudent(Student *head, int studentID, int newUnits, float newGPA);
+// void displayAllRecords(Student *head);
 
 int main()
 {
@@ -39,16 +45,16 @@ int main()
         switch (n)
         {
         case 1:
-            addStudent(Student * &head, const string &name, int studentID, int units, float gpa, const string &securityCode);
+            addStudent(student);
             break;
 
         case 2:
-            bool deletestudent(Student * &head, int studentID);
+            bool deletestudent(student);
             break;
 
         case 3:
-            Student *searchStudent(Student * head, int studentID);
-
+            Student *searchStudent(student); // نکته داره این . فرق ارجاع پونتر با رفرنس و ارجاع پوینتر
+            // ??????????????????????
             break;
 
         case 4:
@@ -75,14 +81,81 @@ int main()
     return 0;
 }
 
-void addStudent(Student *&head, const string &name, int studentID, int units, float gpa, const string &securityCode)
+void addStudent(Student *&head)
 {
+    Student *newstu = new Student;
+    cin >> newstu->name;
+    cin >> newstu->studentID;
+    cin >> newstu->units;
+    cin >> newstu->gpa;
+    cin >> newstu->securityCode;
+    newstu->next = nullptr;
+
+    if (head->next == nullptr)
+    {
+        head = newstu;
+    }
+    Student *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newstu;
 }
-bool deletestudent(Student *&head, int studentID)
+bool deletestudent(Student *&head)
 {
+    int studentid = 0;
+    cin >> studentid;
+
+    if (head == nullptr)
+        return false;
+
+    // ????????????????
+    // ????????????????
+
+    Student *temp = head;
+    while (temp->studentID != studentid && temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    if (temp->studentID == studentid)
+    {
+        Student *temp2 = head;
+        while (temp2->next != &*temp && temp2->next != nullptr) // or *&
+        {
+            temp2 = temp2->next;
+        }
+
+        if (temp2->next == &*temp)
+        {
+            temp2 = temp->next;
+            delete temp;
+        }
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
-Student *searchStudent(Student *head, int studentID)
+Student *searchStudent(Student *head)
 {
+    int studentid = 0;
+    cin >> studentid;
+    Student *temp = head;
+    while (temp->studentID != studentid && temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    if (temp->studentID == studentid)
+    {
+        return temp;
+    }
+    else
+    {
+        return nullptr;
+    }
 }
 bool updateStudent(Student *head, int studentID, int newUnits, float newGPA)
 {
